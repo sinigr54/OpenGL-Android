da5a52aaef35aa9b8619e1aa6b18b0b0c4a14633
Александр Чех <support@mobiledevelop.ru>
2018-04-10 08:12:40 +0300

    Add more useful color and lighting structure

--- app/src/main/assets/cube_fragment_shader.glsl
+++ app/src/main/assets/cube_fragment_shader.glsl
@@ -5,29 +5,41 @@ in vec3 outFragmentPosition;
 
 out vec4 outColor;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPosition;
 uniform vec3 viewPosition;
 
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Light light;
+uniform Material material;
+
 void main() {
-	float ambientStrength = 0.2f;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = light.ambient * material.ambient;
 
     vec3 normal = normalize(outNormal);
-    vec3 lightDirection = normalize(lightPosition - outFragmentPosition);
+    vec3 lightDirection = normalize(light.position - outFragmentPosition);
 
     float diffuseValue = max(dot(normal, lightDirection), 0.0);
-    vec3 diffuse = diffuseValue * lightColor;
-
-    float specularStrenght = 1.0f;
+    vec3 diffuse = light.diffuse * (diffuseValue * material.diffuse);
 
     vec3 viewDirection = normalize(viewPosition - outFragmentPosition);
     vec3 reflectDirection = reflect(-lightDirection, normal);
 
-    float specularValue = pow(max(dot(viewDirection, reflectDirection), 0.0), 32.0f);
-    vec3 specular = specularStrenght * specularValue * lightColor;
+    float specularValue = pow(max(dot(viewDirection, reflectDirection), 0.0), material.shininess);
+    vec3 specular = light.specular * (specularValue * material.specular);
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = ambient + diffuse + specular;
     outColor = vec4(result, 1.0f);
 }
--- app/src/main/cpp/application/renderer/Renderer.cpp
+++ app/src/main/cpp/application/renderer/Renderer.cpp
@@ -71,9 +71,12 @@ GLfloat gVertices[] = {
 float screenWidth = 0.0f;
 float screenHeight = 0.0f;
 
-const Camera camera(glm::vec3(-6.5f, 2.0f, -2.5f));
+const Camera camera(glm::vec3(0.0f, 2.0f, 4.0f));
+//const Camera camera(glm::vec3(0.0f, 5.0f, 3.0f));
 
-const glm::vec3 lightPosition(1.0f, 1.0f, 1.0f);
+glm::vec3 lightPosition(0.0f, 1.0f, 0.0f);
+
+double delta = 0.0;
 
 Renderer::Renderer(AAssetManager *assetManager) : assetManager(assetManager) {
 
@@ -144,17 +147,44 @@ void Renderer::onDrawFrame() {
     GLint viewLocation = glGetUniformLocation(sceneShaderProgram, "view");
     GLint projectionLocation = glGetUniformLocation(sceneShaderProgram, "projection");
 
-    GLint objectColorLocation = glGetUniformLocation(sceneShaderProgram, "objectColor");
-    GLint lightColorLocation = glGetUniformLocation(sceneShaderProgram, "lightColor");
-    GLint lightPositionLocation = glGetUniformLocation(sceneShaderProgram, "lightPosition");
     GLint viewPositionLocation = glGetUniformLocation(sceneShaderProgram, "viewPosition");
+
+    GLint ambientMaterialLocation = glGetUniformLocation(sceneShaderProgram, "material.ambient");
+    GLint diffuseMaterialLocation = glGetUniformLocation(sceneShaderProgram, "material.diffuse");
+    GLint specularMaterialLocation = glGetUniformLocation(sceneShaderProgram, "material.specular");
+    GLint shininessMaterialLocation = glGetUniformLocation(sceneShaderProgram,
+                                                           "material.shininess");
+
+    GLint ambientLightLocation = glGetUniformLocation(sceneShaderProgram, "light.ambient");
+    GLint diffuseLightLocation = glGetUniformLocation(sceneShaderProgram, "light.diffuse");
+    GLint specularLightLocation = glGetUniformLocation(sceneShaderProgram, "light.specular");
+    GLint lightPositionLocation = glGetUniformLocation(sceneShaderProgram, "light.position");
 
     glm::mat4 cubeModel;
 
+    /*glm::vec3 lightColor(
+            static_cast<const float &>(sin(delta * 2.0f)),
+            static_cast<const float &>(sin(delta * 0.7f)),
+            static_cast<const float &>(sin(delta * 1.3f))
+    );
+
+    glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
+    glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);*/
+
+    glUniform3f(ambientLightLocation, 1.0f, 1.0f, 1.0f);
+    glUniform3f(diffuseLightLocation, 1.0f, 1.0f, 1.0f);
+    glUniform3f(specularLightLocation, 1.0f, 1.0f, 1.0f);
+
+    // Gold material
+    glUniform3f(ambientMaterialLocation, 0.24725f, 0.1995f, 0.0745f);
+    glUniform3f(diffuseMaterialLocation, 0.75164f, 0.60648f, 0.22648f);
+    glUniform3f(specularMaterialLocation, 0.628281f, 0.555802f, 0.366065f);
+    glUniform1f(shininessMaterialLocation, 32.0f);
+
     glUniform3f(lightPositionLocation, lightPosition.x, lightPosition.y, lightPosition.z);
-    glUniform3f(objectColorLocation, 1.0f, 0.5f, 0.31f);
-    glUniform3f(lightColorLocation, 1.0f, 1.0f, 1.0f);
-    glUniform3f(viewPositionLocation, camera.getPosition().x, camera.getPosition().y, camera.getPosition().z);
+
+    glUniform3f(viewPositionLocation, camera.getPosition().x, camera.getPosition().y,
+                camera.getPosition().z);
 
     glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
@@ -186,4 +216,12 @@ void Renderer::onDrawFrame() {
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glBindVertexArray(0);
+
+    lightPosition.x = static_cast<float>(sin(delta));
+    lightPosition.z = static_cast<float>(cos(delta));
+
+    delta += 0.02;
+    if (delta > 10000.0) {
+        delta = 0.0;
+    }
 }
